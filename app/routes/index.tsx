2d49786301cc82ff58518eab911dfef8f15b98e2
Matthew Burfield <m1.burfield@gmail.com>
2022-11-27 19:34:18 +1000

    init

---
+++ app/routes/index.tsx
@@ -0,0 +1,31 @@
+import { Link } from "@remix-run/react";
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+
+
+
+      <ul>
+        <li>
+          <Link to="/page-two">Page Two</Link>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ app/routes/page-two.tsx
@@ -0,0 +1,30 @@
+import { Link } from "@remix-run/react";
+
+export default function PageTwo() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <Link to="/" className="text-3xl font-bold underline">
+            Home
+          </Link>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
